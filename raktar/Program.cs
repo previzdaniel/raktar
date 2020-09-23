f045edeccbc9e9f10bdc37439739b3fd63d473e8
dani.2000.2x@gmail.com <dani.2000.2x@gmail.com>
2020-09-23 20:51:27 +0200

    beolvasás nem működik

--- raktar/Program.cs
+++ raktar/Program.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace raktar
 {
     class Program
     {
+        static List<Termek> termekek = new List<Termek>();
+
         static void Main(string[] args)
         {
-
+            StreamReader raktar = new StreamReader("raktar.csv");
+            while (!raktar.EndOfStream)
+            {
+                string[] sor = raktar.ReadLine().Split(';');
+                for (int i = 0; i < termekek.Count; i++)
+                {
+                    
+                }
+            }
+            raktar.Close();
 
             Console.ReadKey();
         }
